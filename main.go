6269e5f94f2d9ec6f2f6bcccd5bd64b9f5b5c4b4
licheng10113 <2501093733@qq.com>
2022-11-22 23:21:46 +0800

    优化测试!

--- main.go
+++ main.go
@@ -1,43 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"github.com/fwhezfwhez/errorx"
-	"github.com/xtaci/kcp-go/v5"
-	"io"
-	"io-game-go/decoder"
-	"log"
-	"net"
-)
-
 func main() {
-	log.SetFlags(log.LstdFlags + log.Lshortfile)
-	log.Println("kcp listens on 10000")
-
-	lis, err := kcp.ListenWithOptions(":10000", nil, 10, 3)
-	if err != nil {
-		panic(err)
-	}
-	for {
-		conn, e := lis.AcceptKCP()
-		if e != nil {
-			panic(e)
-		}
-		go func(conn net.Conn) {
-			var buffer = make([]byte, 1024, 1024)
-			for {
-				// 读取长度 n
-				n, e := conn.Read(buffer)
-				if e != nil {
-					if e == io.EOF {
-						break
-					}
-					fmt.Println(errorx.Wrap(e))
-					break
-				}
-				decoder.GetDecoder().DecoderBytes(buffer[:n])
-				//fmt.Println("receive from client:", buffer[:n])
-			}
-		}(conn)
-	}
+	//// 默认的消息实现: DefaultMessage
+	//router.AddFunc(router.GetMerge(0, 1), func(msg interface{}) {
+	//	log.Println("收到消息: ", msg)
+	//})
+	//
+	//server := core.NewGameServer()
+	//server.Run()
 }
