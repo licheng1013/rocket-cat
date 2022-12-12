e58d1aee78bc468b07d36934179d7ae611a0482f
licheng1013 <2501093733@qq.com>
2022-12-12 09:41:42 +0800

    增加测试功能!

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
-	//// 默认的消息实现: DefaultMessage
-	//router.AddFunc(router.GetMerge(0, 1), func(msg interface{}) {
-	//	log.Println("收到消息: ", msg)
-	//})
-	//
-	//server := core.NewGameServer()
-	//server.Run()
+	fmt.Println("HelloWorld")
 }
