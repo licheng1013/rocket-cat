efb6dbd589bf914bb4c82532c7fb21568a6433b0
licheng1013 <2501093733@qq.com>
2023-03-03 11:44:19 +0800

    调整测试案例!

--- core/connect/kcp_test.go
+++ core/connect/kcp_test.go
@@ -1,7 +1,6 @@
 package connect
 
 import (
-	"fmt"
 	"github.com/xtaci/kcp-go/v5"
 	"io"
 	"log"
@@ -27,17 +26,13 @@ func TestKcpServer(t *testing.T) {
 func KcpClient(channel chan int) {
 	//log.Println("客户端监听:" + Addr)
 	if client, err := kcp.DialWithOptions(Addr, nil, 10, 3); err == nil {
-		index := 1
 		for {
-			data := HelloMsg + fmt.Sprint(index)
+			data := HelloMsg
 			buf := make([]byte, len(data))
 			if _, err := client.Write([]byte(data)); err == nil {
 				if _, err := io.ReadFull(client, buf); err == nil {
 					log.Println("获取数据:" + string(buf))
-					index++
-					if index >= 10 {
-						channel <- 0
-					}
+					channel <- 0
 				} else {
 					log.Fatal(err)
 				}
