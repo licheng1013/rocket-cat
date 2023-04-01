b543b23f15d231faa9bae6e241bd669852dec993
licheng1013 <2501093733@qq.com>
2023-04-01 09:39:38 +0800

    验证调试问题!

--- examples/verify/main.go
+++ examples/verify/main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/licheng1013/rocket-cat/common"
+	"github.com/licheng1013/rocket-cat/connect"
+	"log"
 )
 
 func main() {
-	sm := common.NewSafeMap()
-	sm.Set(1, 10)
-	sm.Set(2, "hello")
-	sm.Set(3, true)
-	fmt.Println(sm.Keys())
-	fmt.Println(sm.Get(2))
-	fmt.Println(sm.Values())
+	var v int64
+	socket := connect.KcpSocket{}
+	socket.OnClose(func(socketId uint32) {
+		// 关闭连接
+		log.Println("关闭连接 -> ", socketId)
+	})
+	socket.Pool = common.NewPool()
+	socket.ListenBack(func(uuid uint32, bytes []byte) []byte {
+		fmt.Println("收到数据:" + string(bytes))
+		v++
+		return []byte("收到数据:" + string(bytes) + fmt.Sprint(v))
+	})
+	socket.ListenAddr("127.0.0.1:12355")
 }
