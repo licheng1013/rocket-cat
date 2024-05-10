cad55ddf3d4ec666a9be714e2d4a0990271a3c88
licheng <2501093733@qq.com>
2024-05-10 19:53:25 +0800

    重构房间匹配功能!

---
+++ room/match.go
@@ -0,0 +1,52 @@
+package room
+
+import "sync"
+
+type Match struct {
+	// 匹配人数多少回调
+	maxMatch int64
+	// 匹配成功回调,参数是匹配成功的玩家id
+	callback func(players []int64)
+	// userId : player
+	players sync.Map
+}
+
+func NewMatchQueue(maxMatch int64, callback func([]int64)) *Match {
+	return &Match{
+		maxMatch: maxMatch,
+		callback: callback,
+	}
+}
+
+func (m *Match) AddMatch(player IPlayer) {
+	m.players.Store(player.UserId(), player)
+	size := 0
+	list := make([]int64, 0)
+	m.players.Range(func(key, value any) bool {
+		if size > 1 {
+			return false
+		}
+		list = append(list, key.(int64))
+		size++
+		return true
+	})
+	if int64(size) >= m.maxMatch { // 匹配成功
+		m.callback(list)
+		for _, v := range list {
+			m.players.Delete(v)
+		}
+	}
+}
+
+func (m *Match) RemoveMatch(player IPlayer) {
+	m.players.Delete(player.UserId())
+}
+
+func (m *Match) GetPlayer() []IPlayer {
+	list := make([]IPlayer, 0)
+	m.players.Range(func(key, value any) bool {
+		list = append(list, value.(IPlayer))
+		return true
+	})
+	return list
+}
